2513bf42edae0c855303dc92cb0321fed443eb1c
Samson Oludare Ogunrinde <ogunrindesamson@gmail.com>
2024-01-10 13:30:28 +0100

    updated the script and css files
    Added some animation in the css to make the notification more appealing and eye catching.

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ function showToast(msg) {
   toast.classList.add("toast");
   toast.innerHTML = msg;
   toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 5000);
 }
